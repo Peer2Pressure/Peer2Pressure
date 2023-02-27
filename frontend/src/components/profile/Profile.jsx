0f8b328c9d4df8982bd07d5b52d84324b1efce12
James T <jamesang@ualberta.ca>
2023-02-27 14:32:13 -0700

    added script that enables communication from apis to be rendered on any component/pages.

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,13 +1,23 @@
 import "./profile.css"
+import useFetch from "../../useFetch"
 
 export default function Profile() {
+
+  // calling the api to get data to be rendered in this component
+  const {data, loading, error} = useFetch("https://random-data-api.com/api/v2/users?size=1&is_xml=true");
+  
+  // check if loading 
+  if (loading) return <h1> Loading... </h1>; // placeholder for now 
+
+  // check if any error generated shown in console
+  if (error) console.log(error);
+
   return (
     <div>
         <div className="profileBox">
             <img class="profileImage" src="/assets/johnDoe.jpg" alt="profile of id.name"/>
             <h1 className="nameTitle">
-                {/* put id.Name here  */}
-                John Doe
+                {data?.first_name} {data?.last_name}
             </h1>
             <button className="manageProfileButton">Manage profile</button>
         </div>
