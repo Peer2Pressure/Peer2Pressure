69f7c6edcecbdb121fbe3c72ff33e265487dbd3b
James T <jamesang@ualberta.ca>
2023-03-01 17:22:28 -0700

    changed name to displayName

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -6,7 +6,7 @@ import { Avatar, Button } from "@mui/material";
 export default function Profile() {
 
   // calling the api to get data to be rendered in this component
-  const {data, loading, error} = useFetch("http://localhost:8000/authors/ea89c93d-4879-450d-9d12-58bb06d484c1");
+  const {data, loading, error} = useFetch("http://localhost:8000/authors/ea89c93d-4879-450d-9d12-58bb06d484c1/");
   
   // check if loading 
   if (loading) return <h1> Loading... </h1>; // placeholder for now 
@@ -21,7 +21,7 @@ export default function Profile() {
             {/* <img class="profileImage" src="/assets/johnDoe.jpg" alt="profile of id.name"/> */}
             <Avatar alt={data?.displayName} src="/assets/johnDoe.jpg" sx={{width:100, height:100}}/>
             <h1 className="nameTitle">
-                {data?.first_name} {data?.last_name}
+                {data?.displayName}
                 {/* {data?.displayName} <-- what we actually need to display*/} 
             </h1>
             <h2 className="gitHubProfile">
