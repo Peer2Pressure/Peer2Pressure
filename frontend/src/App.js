4b8c5c2c73f7c45e9afdc7a58430cfa465f9e85e
James <jamesang@ualberta.ca>
2023-03-21 23:10:30 -0600

    fixed signin and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,16 @@ import Stream  from './components/stream/Stream';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/Home";
 import ProfilePage from './pages/profilePage/ProfilePage';
+import Signin from './pages/login/Signin';
+import Signup from './pages/register/Signup';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Routes>
+          <Route exact path="/signin" element={<Signin/>} />
+          <Route exact path="/signup" element={<Signup/>} />
           <Route exact path="/" element={<Home/>} />
           <Route path="/profilepage" element={<ProfilePage/>} />
         </Routes>
