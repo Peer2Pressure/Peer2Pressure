187cc94f77d8ed34d2289b1e19e37bd8cac04e4e
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-03-23 13:06:40 -0600

    allowed hosts

--- backend/peer2pressure/settings.py
+++ backend/peer2pressure/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-^uq3h4s&#$w-r^^u7x844q@2$or=71mqm!e4m-g^q0g!el-6$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://peer2pressure.herokuapp.com"]
 
 # Application definition
 
