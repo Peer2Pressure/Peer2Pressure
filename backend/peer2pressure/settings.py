726e1e4aa85c52c3fcd1ec9e56c3c803ecab2708
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-03-26 00:11:13 -0600

    update allowed hosts

--- backend/peer2pressure/settings.py
+++ backend/peer2pressure/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = "django-insecure-^uq3h4s&#$w-r^^u7x844q@2$or=71mqm!e4m-g^q0g!el-6$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://p2psd.herokuapp.com"]
+ALLOWED_HOSTS = ["https://p2psd.herokuapp.com", 
+                 "https://nevil-p2p.herokuapp.com", 
+                 "https://peer2test.herokuapp.com",
+                 "https://www.distribution.social"
+                 ]
 
 # Application definition
 
@@ -56,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "https://nevil-p2p.herokuapp.com", "https://peer2test.herokuapp.com",
+    "https://p2psd.herokuapp.com",
     "https://cmput404-project-app.herokuapp.com",
     "https://www.distribution.social",
     "http://localhost:3000",
