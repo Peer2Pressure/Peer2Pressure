342da51054c3bcc17452710f5d2fa88cabdf5d08
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-03-26 17:33:26 -0600

    css changes

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -7,7 +7,8 @@ import axios from "axios";
 import useGetAuthorData from "../../useGetAuthorData";
 import { v4 as uuidv4 } from 'uuid';
 
-const Share = () => {
+function Share (props) {
+    const {setPostsUpdated} = props;
 
     const [files, setFiles] = useState([]);
     const [content, setContent] = useState("");
@@ -17,7 +18,7 @@ const Share = () => {
     // const [fileURL, setFileURL] = useState(null)
 
 
-    const {data, loading, error, authorID} = useGetAuthorData();
+    const {authorData, loading, error, authorID} = useGetAuthorData();
 
     const handleContentChange = event => {
         setContent(event.target.value);
@@ -52,18 +53,17 @@ const Share = () => {
         const p = axios
         .post(`/authors/${authorID}/inbox/`, {
             "type": "post",
-            "id": `${data.host}/authors/${authorID}/posts/${uuidv4()}`,
+            "id": `${authorData.host}/authors/${authorID}/posts/${uuidv4()}`,
             "contentType": contentType,
             "content": content,
-            "author": data,
+            "author": authorData,
         })
         
         const p2 = p.then((response) => {
+            setPostsUpdated(response.data);
+            setContent("");
             const p3 = getFollowers()
             const p4 = p3.then((response2) => {
-                // console.log("p3", p3)
-                // console.log("r2", response2)
-                // console.log("rd", response.data)
                 const requestPromises = response2.map(endpoint => {
                     axios.post(endpoint, response.data)
                 })
@@ -75,9 +75,9 @@ const Share = () => {
                 .catch((error) => {
                     console.error('Error sending requests:', error);
                 })
-                .finally(() => {
-                    window.location.reload();
-                });
+                // .finally(() => {
+                //     window.location.reload();
+                // });
             }
             )
         })
