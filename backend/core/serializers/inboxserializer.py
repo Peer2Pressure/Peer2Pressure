222e0b990074e3228d8aa4700fb0245a0261f48d
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-03-28 23:25:07 -0600

    handle getting follow requests from inbox

--- backend/core/serializers/inboxserializer.py
+++ backend/core/serializers/inboxserializer.py
@@ -10,6 +10,7 @@ from varname import nameof
 # Local libraries
 from .. models import *
 from ..serializers.postserializer import PostSerializer
+from ..serializers.followerserializer import FollowerSerializer
 from django.core.files.base import ContentFile
 
 class InboxItemsSerializer(serializers.Serializer):
@@ -17,6 +18,20 @@ class InboxItemsSerializer(serializers.Serializer):
     page = serializers.IntegerField(allow_null=True, required=False)
     size = serializers.IntegerField(allow_null=True, required=False)
     items = PostSerializer(many=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if data['page'] is None:
+            data.pop('page')
+        if data['size'] is None:
+            data.pop('size')
+        return data
+
+class InboxFollowRequestSerializer(serializers.Serializer):
+    type = serializers.CharField(default="request" , max_length=10, read_only=True, required=False)
+    page = serializers.IntegerField(allow_null=True, required=False)
+    size = serializers.IntegerField(allow_null=True, required=False)
+    items = FollowerSerializer(many=True)
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
