4f37cc7cd471cd71b0c4896691f26f64a3b5aa94
James <jamesang@ualberta.ca>
2023-04-01 00:11:42 -0600

    tried implementing tabs to segregate regular stream and direct messages (private)

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import * as React from 'react';
 import "./home.css";
 import Share from "../../components/share/Share";
 import Profile from "../../components/profile/Profile";
@@ -9,9 +9,20 @@ import Widgets from "../../components/widgets/Widgets";
 import Post from "../../components/post/Post";
 import NavBar from "../../components/navBar/NavBar";
 import FollowRequest from "../../components/followRequests/followRequest";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+
+
 
 const Home = () => {
   const [postsUpdated, setPostsUpdated] = useState(false);
+  const [tabValue, setTabValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
   return (
     <div className="homeContainer">
       {/* <div className="navBarContainer">
@@ -23,7 +34,23 @@ const Home = () => {
         </div>
         <div className="streamContainer">
           <Share postsUpdated={postsUpdated} setPostsUpdated={setPostsUpdated}/>
-          <Stream postsUpdated={postsUpdated}/>
+          <br></br>
+          <Box sx={{ "& .MuiTab-root": {borderBottom: 1, 
+            borderColor: 'divider',
+            fontWeight: 600,
+            fontFamily: 'Montserrat, sans-serif;',
+            color: "#2C4268" ,}, "& .Mui-selected": {
+              color: "#007BBA",
+            },
+            }}>
+            <Tabs value={tabValue} onChange={handleChange}>
+              <Tab label="Regular Stream" value="1" />
+              <Tab label="Direct Message" value="2" />
+            </Tabs>
+          </Box>
+          {tabValue === "1" && <Stream postsUpdated={postsUpdated} />}
+          {tabValue === "2" && <Stream postsUpdated={postsUpdated} />}
+          
         </div>
         <div className="widgetContainer">
            <Widgets/>
