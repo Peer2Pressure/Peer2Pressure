52a8595a01232a116ccafd6326580294e904f16c
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-04-05 20:33:37 -0600

    testing db connection pool on heroku

--- backend/peer2pressure/settings.py
+++ backend/peer2pressure/settings.py
@@ -109,13 +109,29 @@ WSGI_APPLICATION = "peer2pressure.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'dj_db_conn_pool.backends.postgresql',
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT': os.environ.get("DATABASE_PORT"),
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "dj_db_conn_pool.backends.postgresql",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
